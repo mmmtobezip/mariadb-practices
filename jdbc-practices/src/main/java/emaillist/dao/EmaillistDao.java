26cb9fa9301b6ae87dce4c73f1c1da5ed7b4c090
mmmtobezip <mmmszip@gmail.com>
2024-05-17 17:43:24 +0900

    fix: EmaillistDao findall()

--- jdbc-practices/src/main/java/emaillist/dao/EmaillistDao.java
+++ jdbc-practices/src/main/java/emaillist/dao/EmaillistDao.java
@@ -3,7 +3,10 @@ package emaillist.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import emaillist.vo.EmaillistVo;
 
 public class EmaillistDao {
@@ -100,6 +103,69 @@ public class EmaillistDao {
         System.out.println("error: " + e);
       }
     }
+    return result;
+  }
+
+
+  public List<EmaillistVo> findAll() {
+    List<EmaillistVo> result = new ArrayList<>();
+    Connection conn = null;
+    PreparedStatement pstmt = null;
+    ResultSet rs = null;
+
+    try {
+
+      // 1. JDBC Driver 로딩
+      Class.forName("org.mariadb.jdbc.Driver");
+
+      // 2. 연결하기
+      String url = "jdbc:mariadb://192.168.0.191:3306/webdb?charset=utf8";
+      conn = DriverManager.getConnection(url, "webdb", "webdb");
+
+      // 3. Statement 준비시키기(prepare)
+      String sql =
+          "   select no, first_name, last_name, email" + 
+          "     from emaillist" + 
+          " order by no desc";
+      pstmt = conn.prepareStatement(sql);
+
+      rs = pstmt.executeQuery();
+
+      // 6. 결과 처리
+      while (rs.next()) {
+        Long no = rs.getLong(1);
+        String firstName = rs.getString(2);
+        String lastName = rs.getString(3);
+        String email = rs.getString(4);
+
+        EmaillistVo vo = new EmaillistVo();
+        vo.setNo(no);
+        vo.setFirstName(firstName);
+        vo.setLastName(lastName);
+        vo.setEmail(email);
+
+        result.add(vo);
+      }
+    } catch (ClassNotFoundException e) {
+      System.out.println("드라이버 로딩 실패:" + e);
+    } catch (SQLException e) {
+      System.out.println("error:" + e);
+    } finally {
+      try {
+        if (rs != null) {
+          rs.close();
+        }
+        if (pstmt != null) {
+          pstmt.close();
+        }
+        if (conn != null) {
+          conn.close();
+        }
+      } catch (SQLException e) {
+        System.out.println("error: " + e);
+      }
+    }
+
     return result;
   }
 
