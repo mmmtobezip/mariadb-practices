afead91aa4d7567c9ed6bf7c3ac88f34b327c01d
mmmtobezip <mmmszip@gmail.com>
2024-05-17 13:30:07 +0900

    add: MyDriver

---
+++ jdbc-practices/src/main/java/driver/MyDriverTest.java
@@ -0,0 +1,35 @@
+package driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyDriverTest {
+
+  public static void main(String[] args) {
+
+    Connection connection = null;
+    try {
+      // 1. JDBC Driver 로딩
+      Class.forName("driver.MyDriver");
+      String url = "jdbc:mydb://127.0.0.1:2202/webdb"; // jdbc:dbdriver(내가 만들 예정)
+      connection = DriverManager.getConnection(url, "webdb", "webdb"); // 등록되어있는 mydriver의
+                                                                       // connector를 불러줌.
+
+      System.out.println("success!");
+    } catch (ClassNotFoundException e) {
+      System.out.println("드라이버 로딩 실패:" + e);
+    } catch (SQLException e) {
+      System.out.println("error:" + e);
+    } finally {
+      try {
+        if (connection != null) {
+          connection.close();
+        }
+      } catch (SQLException e) {
+        System.out.println("error: " + e);
+      }
+    }
+  }
+
+}
