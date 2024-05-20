e2461f600ff896bb4c5a94a1cbc27756ae1d20cb
mmmtobezip <mmmszip@gmail.com>
2024-05-20 13:12:02 +0900

    test: testFindAll()

--- jdbc-practices/src/test/java/bookshop/dao/AuthorDaoTest.java
+++ jdbc-practices/src/test/java/bookshop/dao/AuthorDaoTest.java
@@ -1,10 +1,16 @@
 package bookshop.dao;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import bookshop.vo.AuthorVo;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class AuthorDaoTest {
   private static int count = 0;
   private static AuthorDao authorDao = new AuthorDao();
@@ -12,18 +18,25 @@ public class AuthorDaoTest {
   @BeforeAll
   public static void setUp() {
     count = authorDao.findAll().size(); // findAll()로 나온 list의 size
-
   }
 
   @Test
+  @Order(1)
   public void testInsert() {
-    assertTrue(1 - 1 == 0);
+    AuthorVo vo = new AuthorVo();
+    vo.setName("칼세이건");
 
+    Boolean result = authorDao.insert(vo);
+    assertTrue(result);
+  }
+
+  @Test
+  @Order(2)
+  public void testFindAll() {
+    assertEquals(count + 1, authorDao.findAll().size());
   }
 
   @AfterAll
-  public static void cleanUp() {
-
-  }
+  public static void cleanUp() {}
 
 }
