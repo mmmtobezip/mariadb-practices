60147e4239f4a993e38967766e352f0de847f2b3
mmmtobezip <mmmszip@gmail.com>
2024-05-26 00:59:51 +0900

    feat: UserDao 생성

--- jdbc-practices/src/main/java/bookmall/dao/UserDao.java
+++ jdbc-practices/src/main/java/bookmall/dao/UserDao.java
@@ -8,7 +8,93 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import bookmall.vo.BookVo;
 import bookmall.vo.UserVo;
 
 public class UserDao {
+
+	public int insert(UserVo vo) {
+		int result = 0;
+
+		try(
+			Connection conn = getConnection();
+			PreparedStatement pstmt1 = conn.prepareStatement("insert into user(name, email, password, phone) values(?, ?, ?, ?)");
+			PreparedStatement pstmt2 = conn.prepareStatement("select last_insert_id() from dual");) {
+			
+			pstmt1.setString(1, vo.getName());
+			pstmt1.setString(2, vo.getEmail());
+			pstmt1.setString(3, vo.getPassword());
+			pstmt1.setString(4, vo.getPhone());
+			
+			result = pstmt1.executeUpdate();
+			
+			ResultSet rs = pstmt2.executeQuery();
+			
+			vo.setNo(rs.next() ? rs.getLong(1) : null);
+			rs.close();
+		} catch (SQLException e) {
+			System.out.println("ERROR: " + e);
+		}
+		return result;
+	}
+
+	public int deleteByNo(Long no) {
+		int result = 0;
+		
+		try (
+			Connection conn = getConnection();
+		    PreparedStatement pstmt = conn.prepareStatement("delete from user where no = ?");) {
+		    
+			pstmt.setLong(1, no);
+		    result = pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			System.out.println("ERROR: " + e);
+		}
+		return result;
+	}
+
+	public List<UserVo> findAll() {
+		List<UserVo> result = new ArrayList<>();
+		
+		try (
+			Connection conn = getConnection();
+			PreparedStatement pstmt = conn.prepareStatement("select no, name, phone, email, password from user order by no");) {
+			
+			ResultSet rs = pstmt.executeQuery();
+			while(rs.next()) {
+				Long no = rs.getLong(1);
+				String name = rs.getString(2);
+				String phone = rs.getString(3);
+				String email = rs.getString(4);
+				String password = rs.getString(5);
+				
+				UserVo vo = new UserVo(name, phone, email, password);
+				vo.setNo(no);
+				vo.setName(name);
+				vo.setPhone(phone);
+				vo.setEmail(email);
+				vo.setPassword(password);
+				
+				result.add(vo);
+			}
+			rs.close();
+		} catch (SQLException e) {
+			System.out.println("ERROR: " + e);
+		}
+		return result;
+	}
+	
+	private Connection getConnection() throws SQLException {
+		Connection conn = null;
+		
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			String url = "jdbc:mariadb://192.168.64.2:3306/bookmall?charset=utf8";
+			conn = DriverManager.getConnection(url, "bookmall", "bookmall");
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패: " + e);
+		}
+		return conn;
+	}
 }
