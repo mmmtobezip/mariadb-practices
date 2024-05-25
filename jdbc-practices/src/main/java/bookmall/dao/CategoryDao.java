9b56d0ddaa324deda9da0e4198220bac36c9a93d
mmmtobezip <mmmszip@gmail.com>
2024-05-26 00:59:13 +0900

    feat: CategoryDao 생성

--- jdbc-practices/src/main/java/bookmall/dao/CategoryDao.java
+++ jdbc-practices/src/main/java/bookmall/dao/CategoryDao.java
@@ -8,7 +8,83 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import bookmall.vo.BookVo;
 import bookmall.vo.CategoryVo;
+import bookmall.vo.UserVo;
 
 public class CategoryDao {
+	public int insert(CategoryVo vo) {
+		int result = 0;
+
+		try(
+			Connection conn = getConnection();
+			PreparedStatement pstmt1 = conn.prepareStatement("insert into category(name) values (?)");
+			PreparedStatement pstmt2 = conn.prepareStatement("select last_insert_id() from dual");) {
+			
+			pstmt1.setString(1, vo.getName());
+			result = pstmt1.executeUpdate();
+			
+			ResultSet rs = pstmt2.executeQuery();
+			
+			vo.setNo(rs.next() ? rs.getLong(1) : null);
+			rs.close();
+		} catch (SQLException e) {
+			System.out.println("ERROR: " + e);
+		}
+		return result;
+	}
+
+	public int deleteByNo(Long no) {
+		int result = 0;
+		
+		try (
+			Connection conn = getConnection();
+		    PreparedStatement pstmt = conn.prepareStatement("delete from category where no = ?");) {
+		    
+			pstmt.setLong(1, no);
+		    result = pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			System.out.println("ERROR: " + e);
+		}
+		return result;
+	}
+
+	public List<CategoryVo> findAll() {
+		List<CategoryVo> result = new ArrayList<>();
+		
+		try (
+			Connection conn = getConnection();
+			PreparedStatement pstmt = conn.prepareStatement("select no, name from category order by no");) {
+			
+			ResultSet rs = pstmt.executeQuery(); 
+			while(rs.next()) {
+				Long no = rs.getLong(1);
+				String name = rs.getString(2);
+				
+				CategoryVo vo = new CategoryVo(name);
+				vo.setNo(no);
+				vo.setName(name);
+				
+				result.add(vo);
+			}
+			rs.close();
+		} catch (SQLException e) {
+			System.out.println("ERROR: " + e);
+		}
+		return result;
+	}
+	
+	private Connection getConnection() throws SQLException {
+		Connection conn = null;
+		
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			String url = "jdbc:mariadb://192.168.64.2:3306/bookmall?charset=utf8";
+			conn = DriverManager.getConnection(url, "bookmall", "bookmall");
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패: " + e);
+		}
+		return conn;
+	}
 }
