caec9aea0ff3a67d699c209997b3370d1f3fcdf4
mmmtobezip <mmmszip@gmail.com>
2024-05-26 01:00:28 +0900

    feat: CartVo 생성

--- jdbc-practices/src/main/java/bookmall/vo/CartVo.java
+++ jdbc-practices/src/main/java/bookmall/vo/CartVo.java
@@ -1,5 +1,33 @@
 package bookmall.vo;
 
 public class CartVo {
+	private Integer quantity;
+	private Long userNo;
+	private Long bookNo;
+	private String bookTitle;
 	
+	public Integer getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	public Long getUserNo() {
+		return userNo;
+	}
+	public void setUserNo(Long userNo) {
+		this.userNo = userNo;
+	}
+	public Long getBookNo() {
+		return bookNo;
+	}
+	public void setBookNo(Long bookNo) {
+		this.bookNo = bookNo;
+	}
+	public String getBookTitle() {
+		return bookTitle;
+	}
+	public void setBookTitle(String bookTitle) {
+		this.bookTitle = bookTitle;
+	}
 }
