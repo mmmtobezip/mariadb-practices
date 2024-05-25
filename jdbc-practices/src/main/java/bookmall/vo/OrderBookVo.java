671b0b6ab0cd020b4740d6c0e1d0369d0c3eaa51
mmmtobezip <mmmszip@gmail.com>
2024-05-26 01:01:13 +0900

    feat: OrderBookVo 생성

--- jdbc-practices/src/main/java/bookmall/vo/OrderBookVo.java
+++ jdbc-practices/src/main/java/bookmall/vo/OrderBookVo.java
@@ -1,5 +1,40 @@
 package bookmall.vo;
 
 public class OrderBookVo {
-
+	private Long bookNo;
+	private Long orderNo;
+	private Integer quantity;
+	private Integer price;
+	private String bookTitle;
+	
+	public Long getBookNo() {
+		return bookNo;
+	}
+	public void setBookNo(Long bookNo) {
+		this.bookNo = bookNo;
+	}
+	public Long getOrderNo() {
+		return orderNo;
+	}
+	public void setOrderNo(Long orderNo) {
+		this.orderNo = orderNo;
+	}
+	public Integer getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	public String getBookTitle() {
+		return bookTitle;
+	}
+	public void setBookTitle(String bookTitle) {
+		this.bookTitle = bookTitle;
+	}
 }
